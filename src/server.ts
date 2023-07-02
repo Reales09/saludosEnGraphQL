057b053f4569a2da272697224e130c3edf63ed00
Reales <rclarence123@gmail.com>
2023-07-01 22:49:44 -0500

    Se configura server con express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hola mundo en el curso de GrahQL');
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use('*', cors());
+app.use(compression());
+app.use('/',(re, res)=>{
+    res.send('Bienvenidos al curso de GraphQL');
+})
+
+const PORT = 5300;
+
+app.listen(
+    { port : PORT},
+    () => console.log(`hola mun API GrahpWL http://localhost:${PORT}`)
+    )
